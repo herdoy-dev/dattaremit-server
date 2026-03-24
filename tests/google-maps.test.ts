8ec6244d529f0b8ff869bf4e88da43b407290c30
prateek00077 <pandeyprateeek560@gmail.com>
2026-03-24 10:25:33 +0530

    fixed tests

--- tests/google-maps.test.ts
+++ tests/google-maps.test.ts
@@ -102,6 +102,7 @@ describe("Google Maps Endpoints", () => {
         undefined,
         "550e8400-e29b-41d4-a716-446655440000",
         undefined,
+        undefined,
         undefined
       );
     });
